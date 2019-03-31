2f81054a1d4294d890d4c56bfd4b8b259a5a0901
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-03-31 21:06:42 +0200

    Update license note

--- sources/demo.ts
+++ sources/demo.ts
@@ -1,6 +1,6 @@
-/**
- * @license MIT
- * @author Sophie Bremer
- */
+/*---------------------------------------------------------------------------*/
+/* Copyright (c) ORDBOK contributors. All rights reserved.                   */
+/* Licensed under the MIT License. See the LICENSE file in the project root. */
+/*---------------------------------------------------------------------------*/
 
 export * from './demo/index';
