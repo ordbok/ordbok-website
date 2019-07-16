80e05fd38143384330f5980a14f8b080f92cf215
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-07-16 22:37:00 +0200

    Add index plugin

--- rjsconfig.js
+++ rjsconfig.js
@@ -3,6 +3,7 @@
     out: "docs/demo.js",
     paths: {
         "@ordbok/core": "node_modules/@ordbok/core/dist/client",
+        "@ordbok/index-plugin": "node_modules/@ordbok/index-plugin/dist/client",
         "demo": "docs/demo"
     }
 })
