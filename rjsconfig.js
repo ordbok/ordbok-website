49b4d48ab9521d0f817523f58df7072ff2f6929f
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-08-13 20:17:25 +0200

    Update package

--- rjsconfig.js
+++ rjsconfig.js
@@ -2,8 +2,8 @@
     name: "demo",
     out: "docs/demo.js",
     paths: {
-        "@ordbok/core": "node_modules/@ordbok/core/dist/client",
-        "@ordbok/index-plugin": "node_modules/@ordbok/index-plugin/dist/client",
+        "@ordbok/core": "node_modules/@ordbok/core/dist/client/ordbok-core",
+        "@ordbok/index-plugin": "node_modules/@ordbok/index-plugin/dist/client/ordbok-index-plugin",
         "demo": "docs/demo"
     }
 })
